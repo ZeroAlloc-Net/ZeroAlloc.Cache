bf2675742b03f8c546f02e2c19763c15e7a12b96
Marcel Roozekrans <marcel@roozekrans.nl>
2026-04-19 18:29:46 +0200

    test(generator): ZC0002 reference type key parameter diagnostic tests

--- tests/ZeroAlloc.Cache.Generator.Tests/DiagnosticTests.cs
+++ tests/ZeroAlloc.Cache.Generator.Tests/DiagnosticTests.cs
@@ -46,4 +46,66 @@ public sealed class DiagnosticTests
         var diags = await TestHelper.GetDiagnostics(source);
         diags.Should().NotContain(d => string.Equals(d.Id, "ZC0001", System.StringComparison.Ordinal));
     }
+
+    [Fact]
+    public async Task ZC0002_ReferenceTypeParam_EmitsWarning()
+    {
+        const string source = """
+            using ZeroAlloc.Cache;
+            using System.Threading;
+            using System.Threading.Tasks;
+            namespace T;
+            public class ProductFilter { } // reference type
+            [Cache(TtlMs = 30_000)]
+            public interface IMyService
+            {
+                ValueTask<string> GetAsync(ProductFilter filter, CancellationToken ct);
+            }
+            """;
+
+        var diags = await TestHelper.GetDiagnostics(source);
+        var zc0002 = diags.Where(d => string.Equals(d.Id, "ZC0002", StringComparison.Ordinal)).ToList();
+        zc0002.Should().HaveCount(1);
+        var d0 = zc0002.First();
+        d0.Severity.Should().Be(DiagnosticSeverity.Warning);
+        d0.GetMessage().Should().Contain("filter").And.Contain("GetAsync");
+    }
+
+    [Fact]
+    public async Task ZC0002_NotEmitted_ForString()
+    {
+        const string source = """
+            using ZeroAlloc.Cache;
+            using System.Threading;
+            using System.Threading.Tasks;
+            namespace T;
+            [Cache(TtlMs = 30_000)]
+            public interface IMyService
+            {
+                ValueTask<string> GetAsync(string id, CancellationToken ct);
+            }
+            """;
+
+        var diags = await TestHelper.GetDiagnostics(source);
+        diags.Should().NotContain(d => string.Equals(d.Id, "ZC0002", StringComparison.Ordinal));
+    }
+
+    [Fact]
+    public async Task ZC0002_NotEmitted_ForValueTypes()
+    {
+        const string source = """
+            using ZeroAlloc.Cache;
+            using System.Threading;
+            using System.Threading.Tasks;
+            namespace T;
+            [Cache(TtlMs = 30_000)]
+            public interface IMyService
+            {
+                ValueTask<string> GetAsync(int id, CancellationToken ct);
+            }
+            """;
+
+        var diags = await TestHelper.GetDiagnostics(source);
+        diags.Should().NotContain(d => string.Equals(d.Id, "ZC0002", StringComparison.Ordinal));
+    }
 }
